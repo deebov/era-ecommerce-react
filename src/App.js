6485b5af7eae967cfb2c9cd14341b6c4f48dd2ab
Dilshod Turobov <deebov@yandex.com>
2018-11-26 19:46:38 +0500

    render ProductList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './containers/Layout/Layout';
+import ProductsList from './containers/ProductsList/ProductsList';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          
+          <ProductsList />
         </Layout>
       </div>
     );
