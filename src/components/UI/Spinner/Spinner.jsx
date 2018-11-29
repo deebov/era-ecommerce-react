45ff0d49a2a65631999bba83c28bda52f167c5f7
Dilshod Turobov <deebov@yandex.com>
2018-11-29 16:55:53 +0500

    implement the type prop

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './Spinner.module.css';
 
-const Spinner = () => {
+const Spinner = ({ type = 'big' }) => {
   return (
     <div className={styles.Container}>
-      <span className={styles.Spinner} />
+      <span className={[styles.Spinner, styles[type]].join(' ')} />
     </div>
   );
 };
