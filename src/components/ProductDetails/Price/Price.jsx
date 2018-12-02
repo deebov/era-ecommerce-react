28fd7b04dd6c087b092b6d78775c4b139104471d
Dilshod Turobov <deebov@yandex.com>
2018-12-02 21:07:55 +0500

    add prefix ProductThum to Price component

--- src/components/ProductDetails/Price/Price.jsx
+++ src/components/ProductDetails/Price/Price.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import styles from './Price.module.css';
-import Prices from '../../ProductThumb/Prices/Prices';
+import ProductThumbPrices from '../../ProductThumb/Prices/Prices';
 
 const Price = ({ price, oldPrice }) => {
   return (
     <div className={styles.Price}>
-      <Prices price={price} oldPrice={oldPrice} fSize={24} />
+      <ProductThumbPrices price={price} oldPrice={oldPrice} fSize={24} />
     </div>
   );
 };
