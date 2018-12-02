4cd7ecb0cc7ba42b59d46494e43f3f8b81ebde8f
Dilshod Turobov <deebov@yandex.com>
2018-12-02 21:10:18 +0500

    show Header component

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import Header from '../../components/Header/Header';
-import FooterWithCopyright from '../../components/FooterWithCopyright/FooterWithCopyright';
+// import FooterWithCopyright from '../../components/FooterWithCopyright/FooterWithCopyright';
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        {/* <Header /> */}
+        <Header />
         <main>{this.props.children}</main>
         {/* <FooterWithCopyright /> */}
       </div>
