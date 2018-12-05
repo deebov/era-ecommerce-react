d919c3365c456cf00dec9bfd19171fb321266712
Dilshod Turobov <deebov@yandex.com>
2018-12-05 21:41:00 +0500

    change link URLs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Icon from '../UI/Icon/Icon';
 import style from './Header.module.css';
 import NavLinkItem from '../UI/NavLinkItem/NavLinkItem';
 import Logo from '../UI/Logo/Logo';
+import { LANDING } from '../../constants/routes';
+
 const Header = () => {
   return (
     <header className={style.Header}>
       <div className={style.Logo}>
-        <Logo />
+        <Link to={LANDING}>
+          <Logo />
+        </Link>
       </div>
       <div className={style.NavItems}>
-        <NavLinkItem url="/" exact>
+        <NavLinkItem url={LANDING} exact>
           Home
         </NavLinkItem>
         <NavLinkItem url="/shop">Shop</NavLinkItem>
@@ -19,11 +24,11 @@ const Header = () => {
         <NavLinkItem url="/contact">Contact</NavLinkItem>
       </div>
       <div className={style.IconBox}>
-        <Icon icon="magnifier" className={style.Icon} />
-        <Icon icon="user" className={style.Icon} />
-        <Icon icon="heart" className={style.Icon} />
-        <Icon icon="cart" className={style.Icon} />
-        <Icon icon="menu" className={style.Icon} />
+        <Icon icon="magnifier" className={style.Icon} fontSize={18} />
+        <Icon icon="user" className={style.Icon} fontSize={18} />
+        <Icon icon="heart" className={style.Icon} fontSize={18} />
+        <Icon icon="cart" className={style.Icon} fontSize={18} />
+        <Icon icon="menu" className={style.Icon} fontSize={18} />
       </div>
     </header>
   );
