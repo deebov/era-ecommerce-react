8a11f29733432be8b83a74b8e02c04cc4f995094
Dilshod Turobov <deebov@yandex.com>
2018-12-05 21:40:38 +0500

    add logic from FormHandlersContext

--- src/components/ProductDetails/Buttons/Buttons.jsx
+++ src/components/ProductDetails/Buttons/Buttons.jsx
@@ -4,6 +4,7 @@ import Counter from '../../UI/Counter/Counter';
 import Button from '../../UI/Button/Button';
 import AddToWishList from '../../AddToWishList/AddToWishList';
 import styles from './Buttons.module.css';
+import { FormHandlersContext } from '../../../containers/ProductFull/ProductFull';
 
 const Buttons = ({
   addedToCart,
@@ -13,13 +14,33 @@ const Buttons = ({
 }) => {
   return (
     <form action="">
-      <div className={styles.Buttons}>
-        <Counter incClicked={increasedCounter} decClicked={decreasedCounter} />
-        <Button theme="big" clicked={addedToCart}>
-          add to cart
-        </Button>
-        <AddToWishList clicked={addedToWishlist} />
-      </div>
+      <FormHandlersContext.Consumer>
+        {({
+          incClicked,
+          decClicked,
+          onChange,
+          onBlur,
+          onSubmit,
+          count,
+          max,
+          onSale
+        }) => (
+          <div className={styles.Buttons}>
+            <Counter
+              incClicked={incClicked}
+              decClicked={decClicked}
+              onChange={onChange}
+              onBlur={onBlur}
+              value={count}
+              max={max}
+            />
+            <Button theme="big" clicked={onSubmit} disabled={!onSale}>
+              add to cart
+            </Button>
+            <AddToWishList clicked={addedToWishlist} />
+          </div>
+        )}
+      </FormHandlersContext.Consumer>
     </form>
   );
 };
