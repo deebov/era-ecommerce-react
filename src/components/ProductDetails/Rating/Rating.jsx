9284ddd45bfff69a75d5467db5770031c8ac24e5
Dilshod Turobov <deebov@yandex.com>
2018-12-05 21:41:09 +0500

    comment reviews

--- src/components/ProductDetails/Rating/Rating.jsx
+++ src/components/ProductDetails/Rating/Rating.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import styles from './Rating.module.css';
 import RatingStars from '../../ProductThumb/RatingStars/RatingStars';
 
-const Rating = ({ reviews, rating }) => {
-  const reviewsLen = reviews.length;
+const Rating = ({ rating }) => {
+  // const reviewsLen = reviews.length;
 
   return (
     <div className={styles.Rating}>
       <RatingStars rating={rating} theme="yellow" />
       <span className={styles.ReviewsCount}>
-        {reviewsLen} Review{reviewsLen > 1 ? 's' : ''}
+        {/* {reviewsLen} Review{reviewsLen > 1 ? 's' : ''} */}
       </span>
     </div>
   );
