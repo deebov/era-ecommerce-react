347f8ff0a5a477b32d1119cb8dd60beb8f5ec92f
Dilshod Turobov <deebov@yandex.com>
2018-12-05 21:35:05 +0500

    set up firebase settings

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,24 @@
+import app from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.db = app.firestore();
+
+    const settings = { timestampsInSnapshots: true };
+    this.db.settings(settings);
+  }
+}
+
+export default Firebase;
