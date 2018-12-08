80d8a38f98cb7274e2d9961d8f80a71e9d29e4ce
Dilshod Turobov <deebov@yandex.com>
2018-12-08 20:18:35 +0500

    add className props

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './Spinner.module.css';
 
-const Spinner = ({ type = 'big' }) => {
+const Spinner = ({ type = 'big', className = '' }) => {
   return (
     <div className={styles.Container}>
-      <span className={[styles.Spinner, styles[type]].join(' ')} />
+      <span className={[styles.Spinner, styles[type], className].join(' ')} />
     </div>
   );
 };
