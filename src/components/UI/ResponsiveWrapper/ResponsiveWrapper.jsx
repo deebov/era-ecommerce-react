15b8852597a7e378751c858d6d5b39180e71c9dc
Dilshod Turobov <deebov@yandex.com>
2018-12-08 16:29:27 +0500

    file has been moved from containers to UI

--- src/components/UI/ResponsiveWrapper/ResponsiveWrapper.jsx
+++ src/components/UI/ResponsiveWrapper/ResponsiveWrapper.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import styles from './ResponsiveWrapper.module.css';
 
 const ResponsiveWrapper = props => {
-  return <div className={styles.ResponsiveWrapper}>{props.children}</div>;
+  const classNames = [styles.ResponsiveWrapper];
+  if (props.loading) {
+    classNames.push(styles.Loading);
+  }
+  return <div className={classNames.join(' ')}>{props.children}</div>;
 };
 
 export default ResponsiveWrapper;
 
 /**
- * This function returns the className 
+ * This function returns the className
  * so you can use it with any component
  */
 export const getClassName = () => {
   return styles.ResponsiveWrapper;
 };
-
