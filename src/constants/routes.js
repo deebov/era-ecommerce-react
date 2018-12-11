a16166dee6fb14c835ce89e41149ef0825bb1444
Dilshod Turobov <deebov@yandex.com>
2018-12-11 20:32:06 +0500

    add NOT_FOUND route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
 export const LANDING = '/';
 export const ITEM = '/item';
 export const CART = '/cart';
-export const WISHLIST = '/wishlist'
+export const WISHLIST = '/wishlist';
+export const NOT_FOUND = '/404';
