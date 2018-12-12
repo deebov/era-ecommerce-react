e75cc97c3efa611742af107b0789858f937483d9
Dilshod Turobov <deebov@yandex.com>
2018-12-12 11:01:01 +0500

    create 404 page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Redirect, withRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import { NOT_FOUND } from '../../constants/routes';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Page Not Found</title>
+        </Helmet>
+        {this.props.location.pathname !== NOT_FOUND ? (
+          <Redirect to={NOT_FOUND} />
+        ) : null}
+        <h1>Not Found</h1>
+      </div>
+    );
+  }
+}
+
+export default withRouter(NotFound);
