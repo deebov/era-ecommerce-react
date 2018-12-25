dc0a680a709a14b9ca69b23652f7828787a264a6
Dilshod Turobov <deebov@yandex.com>
2018-12-25 11:34:15 +0500

    add auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,3 +5,53 @@ export const switchShowAuth = () => {
     type: actionTypes.SWITCH_SHOW_AUTH
   };
 };
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error
+  };
+};
+
+export const authSuccess = (token, userID) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS
+  };
+};
+
+export const auth = (email, password, method) => async (
+  dispatch,
+  getState,
+  { firebase }
+) => {
+  dispatch(authStart());
+
+  try {
+    let authData = {};
+    let token = null
+    if (method === 'login') {
+      authData = await firebase.doSignInWithEmailAndPassword(email, password);
+      token = await firebase.auth.currentUser.getIdToken()
+      console.log(token);
+      
+    } else {
+      authData = await firebase.doCreateUserWithEmailAndPassword(
+        email,
+        password
+      );
+    }
+    
+    
+    dispatch(authSuccess(authData.idToken, authData.localId));
+  } catch (error) {
+    console.log(error);
+
+    dispatch(authFail(error));
+  }
+};
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -2,6 +2,8 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../utils/index';
 
 const initialState = {
+  token: null,
+  userId: null,
   showAuth: false,
   loading: false,
   error: null
@@ -11,6 +13,16 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.SWITCH_SHOW_AUTH:
       return updateObject(state, { showAuth: !state.showAuth });
+    case actionTypes.AUTH_START:
+      return updateObject(state, { loading: true });
+    case actionTypes.AUTH_FAIL:
+      return updateObject(state, { error: action.error, loading: false });
+    case actionTypes.AUTH_SUCCESS:
+      return updateObject(state, {
+        token: action.token,
+        userId: action.userId,
+        loading: false
+      });
     default:
       return state;
   }
