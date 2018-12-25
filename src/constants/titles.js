a0625ab28784871fc3d4354d119871bef7e6193b
Dilshod Turobov <deebov@yandex.com>
2018-12-25 11:33:03 +0500

    correct the LANGING title

--- src/constants/titles.js
+++ src/constants/titles.js
@@ -1,4 +1,4 @@
-export const LANDING = '`Welcome 🖖';
+export const LANDING = 'Welcome 🖖';
 export const CART = 'Your Cart 🛒';
 export const WISHLIST = 'Your Wishlist ❤️';
 export const NOT_FOUND = 'Page Not Found 🤦🏻‍';
