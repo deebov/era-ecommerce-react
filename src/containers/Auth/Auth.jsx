3c25bd3eda57b32690ccdcc4eb1fe3056223f5ab
Dilshod Turobov <deebov@yandex.com>
2018-12-25 11:34:44 +0500

    add signIn and signUp methods

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -5,12 +5,24 @@ import AuthPresentational from '../../components/Auth/Auth';
 import * as actions from '../../store/actions';
 
 class Auth extends Component {
+  onSignupUserHandler = (e, email, password) => {
+    e.preventDefault();
+
+    this.props.onSignupUser(email, password);
+  };
+  onSigninUserHandler = (e, email, password) => {
+    e.preventDefault();
+
+    this.props.onSigninUser(email, password);
+  };
   render() {
     return (
       <div>
         <AuthPresentational
           visible={this.props.showAuth}
           onSwitchVisible={this.props.onSwitchShowAuth}
+          signupUser={this.onSignupUserHandler}
+          signinUser={this.onSigninUserHandler}
         />
       </div>
     );
@@ -24,7 +36,10 @@ const mapStateToProps = state => {
 };
 const mapDispatchToProps = dispatch => {
   return {
-    onSwitchShowAuth: () => dispatch(actions.switchShowAuth())
+    onSwitchShowAuth: () => dispatch(actions.switchShowAuth()),
+    onSignupUser: (email, password) => dispatch(actions.auth(email, password)),
+    onSigninUser: (email, password) =>
+      dispatch(actions.auth(email, password, 'login'))
   };
 };
 
